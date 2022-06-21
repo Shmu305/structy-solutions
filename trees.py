85f90e6588cb7882c6911ae0b836eb33390519f8
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2022-06-20 21:45:49 -0400

    Update trees.py

--- trees.py
+++ trees.py
@@ -1,5 +1,6 @@
 #037 depth first values
-def depth_first_values(root):
+
+def depth_first_values(root): #iterative
   if not root:
     return []
   res = []
@@ -12,3 +13,11 @@ def depth_first_values(root):
     if curr.left:
       stack.append(curr.left)
   return res
+
+def depth_first_values(root): #recursive
+  if not root:
+    return []
+  left_values = depth_first_values(root.left)
+  right_values = depth_first_values(root.right)
+  return [root.val] + left_values + right_values
+  
