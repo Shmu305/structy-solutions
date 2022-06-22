c03c4d8ba672c3a07cac84717765ec7d1e22d7b6
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2022-06-22 04:47:40 -0400

    Update trees.py

--- trees.py
+++ trees.py
@@ -21,3 +21,19 @@ def depth_first_values(root): #recursive
   right_values = depth_first_values(root.right)
   return [root.val] + left_values + right_values
   
+#038 breadth first values
+from collections import deque
+def breadth_first_values(root):
+  if not root:
+    return []
+  res = []
+  queue = deque([root])
+  while queue:
+    current = queue.popleft()
+    res.append(current.val)
+    
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+  return res
